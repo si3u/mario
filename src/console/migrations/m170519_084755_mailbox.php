16cd4c28a4e4c06a47bf28763f146cb8ed5601a5
Mr.Sieu <dev.phpjs@gmail.com>
2017-05-19 16:58:18 +0700

    .

---
+++ src/console/migrations/m170519_084755_mailbox.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170519_084755_mailbox extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%mailbox}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'email' => $this->string()->notNull(),
+            'phone' => $this->string(),
+            'message' => $this->text()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%mailbox}}');
+    }
+}
